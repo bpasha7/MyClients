1d00cc98da14114b358f5c01d4095b46e80e797f
bpasha <bpasha@mail.ru>
2018-04-03 11:47:17 +0700

    Remove useless files and start testing UserService

---
+++ XUnitTest/UserControllerTest.cs
@@ -0,0 +1,30 @@
+﻿using Data.EF;
+using Data.EF.Entities;
+using Domain.Interfaces.Repositories;
+using Moq;
+using MyClientsBase;
+using MyClientsBase.Controllers;
+using MyClientsBase.Services;
+using System;
+using System.Threading.Tasks;
+using Xunit;
+//
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.TestHost;
+using NetEscapades.AspNetCore.SecurityHeaders.Infrastructure;
+using Microsoft.EntityFrameworkCore;
+
+namespace XUnitTest
+{
+    public class UserControllerTest
+    {
+        public UserControllerTest()
+        {
+            //var mockRepo = new Mock<IUserService>();
+            //mockRepo.Setup(repo => repo.Authenticate("", ""));//.Returns(Task.FromResult());
+            //var controller = new UsersController(mockRepo.Object);
+        }
+    }
+}
